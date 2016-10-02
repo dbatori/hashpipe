82982739d574cdcea1d65b67f080e3cae9463e42
Dani <batoridani@gmail.com>
2016-10-02 21:58:40 +0200

    Add test result formatter (in HashPipe.Format module)

---
+++ lib/hashpipe/format.ex
@@ -0,0 +1,34 @@
+defmodule HashPipe.Format do
+
+  def results(tests, file, module) do
+    fails = Enum.filter tests, fn [_, _, result] -> result != true end
+    num_fails = length(fails)
+    sum = summary(length(tests), num_fails, module, Path.relative_to_cwd(file))
+    sum_color = summary_color(num_fails)
+    "#{IO.ANSI.format [sum_color, :bright, sum]}#{failures fails}"
+  end
+
+  defp pluralize(1, singular), do: singular
+  defp pluralize(_, singular), do: "#{singular}s"
+
+  defp summary(num_tests, num_fails, module, file) do
+    "#{num_tests} #{pluralize num_tests, "HashPipe test"}, " <>
+    "#{num_fails} #{pluralize num_fails, "failure"} " <>
+    "in #{inspect module} (#{file})"
+  end
+
+  defp summary_color(0), do: :green
+  defp summary_color(_num_fails), do: :red
+
+  defp failures(fails) do
+    for {[line_num, test, result], index} <- Enum.with_index(fails, 1), into: "" do
+      num_index_chars = (index |> :math.log10 |> trunc) + 1
+      index_indent = String.duplicate(" ", max(0, 3 - num_index_chars))
+      ~s"""
+      \n\n#{index_indent}#{IO.ANSI.format [:bright, inspect(index), ") line: ", inspect(line_num)]}
+      #{IO.ANSI.format [:red, :bright, test, :cyan, "  -> ", inspect(result)]}
+      """
+    end
+  end
+
+end
